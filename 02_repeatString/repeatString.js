96ccafd2b26c571fc2f653b377cc7489f1fd2c90
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-04-16 20:21:01 +0100

    Complete repeatStrings Js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    output = ""
+    if (num < 0) {
+        output = "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {
+            output += str;
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
