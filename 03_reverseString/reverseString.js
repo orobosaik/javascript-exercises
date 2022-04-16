14198bcc2cd2207bd355dc7f040ddfce19b79e31
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-04-16 20:37:02 +0100

    Complete reverseString Js exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const splitStr = str.split("");
+    const reversedStr = splitStr.reverse();
+    const joinedStr = reversedStr.join("");
+    return joinedStr;
 };
 
 // Do not edit below this line
