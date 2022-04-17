e539a248a60f9ac7f0d0a80953486ff34f72132c
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-04-18 00:14:01 +0100

    Complete removeFromArray Js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+    for (const i of num) {
+        if (!arr.includes(i)) {
+            continue;
+        }else {
+            arr.splice(arr.indexOf(i), 1);
+        }
+    }
 
-};
+    return arr
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
