35a4aa32533e3fb9edb08534956054770fd8201b
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-04-18 12:17:10 +0100

    Complete calculator Js exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (num) {
+  let total = 0;
+  for (const i of num) {
+    total += i
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let total = 1;
+  for (const i of num) {
+    total *= i
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a;
+  for (let i = 1; i < b; i++) {
+		total *= a;
+	}
+	return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for (let i = 1; i <= num; i++) {
+    total *= i
+	}
+	return total;
 };
 
 // Do not edit below this line
