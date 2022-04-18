f23757a348af38c3808679d68d3b2f851cf73886
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-04-18 14:49:12 +0100

    Complete fibonacci Js exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (num) {
+    let n1 = 0, n2 = 1, nextNum;
+    if (num < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 0; i < +num; i++) {
+            nextNum = n1 + n2;
+            n1 = n2;
+            n2 = nextNum;
+        }
+        return n1
+    };
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
