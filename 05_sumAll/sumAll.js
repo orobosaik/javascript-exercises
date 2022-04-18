5b4418af3d7bdd57208277b345e391addd0b97e5
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-04-18 01:48:25 +0100

    Complete sumAll Js exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || typeof(a) !== "number" || typeof(b) !== "number") {
+        sum = "ERROR";
+    } else if (b > a) {
+        for (let i = a; i <= b; i++) {
+            sum = sum + i;
+        }
+    } else if (a > b) {
+        for (let i = b; i <= a; i++) {
+            sum = sum + i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
