7831eb62dc9e16cd536c4fb344a01e3c26ba6f56
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-04-18 10:01:16 +0100

    Complete leapYears Js exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 === 0 && year % 400 === 0) {
+        result = true;
+    } else if (year % 100 === 0) {
+        result = false;
+    } else if (year % 4 !== 0) {
+        result = false;
+    } else if (year % 4 === 0) {
+        result = true;
+    }
+    return result;
 };
 
 // Do not edit below this line
