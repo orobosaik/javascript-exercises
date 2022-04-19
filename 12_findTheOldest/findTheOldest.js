da32015b40cb3dc2da8c6fed29db43628747d719
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-04-19 16:49:32 +0100

    Complete findTheOldest Js exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (list) {
+    let position;
+    let age = 0;
+    let previousAge = 0;
+
+    for (let i = 0; i < list.length; i++) {
+
+        const birth = list[i].yearOfBirth;
+        const death = list[i].yearOfDeath;
+
+        if (!death) {
+            age = new Date().getFullYear() - birth;
+        } else {
+            age = death - birth;
+        }
+
+        if (age > previousAge) {
+            previousAge = age;
+            position = i
+        } else { continue }
+    }
+    return list[position];
 
 };
 
